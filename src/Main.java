819b5744510cff85976bcc37a4e0b6b1edd20151
Slashaershov <127024236+Slashaershov@users.noreply.github.com>
2025-02-20 19:54:22 +0300

    first 3 ex

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,31 @@ public class Main
 {
     public static void main(String[] args)
     {
-        System.out.println("Hello, World!");
+        // exercise 1
+        int myInt = 1;
+        byte myByte = 2;
+        short myShort = 3;
+        long myLong = 4L;
+        float myFloat = 3.5f;
+        double myDouble = 2.3;
+
+        // exercise 2
+        float p1 = 27.12f;
+        long p2 = 987678965549L;
+        float p3 = 2.786f;
+        short p4 = 569;
+        short p5 = -159;
+        short p6 = 27897;
+        byte p7 = 67;
+
+        // exercise 3
+        byte firstClassCount = 24;
+        byte secondClassCount = 27;
+        byte thirdClassCount = 30;
+        short allListCount = 480;
+        int onePeopleListCount = allListCount/
+                (firstClassCount +secondClassCount +thirdClassCount);
+        System.out.println("На каждого ученика рассчитано "+ onePeopleListCount + "листов бумаги" );
     }
+
 }
